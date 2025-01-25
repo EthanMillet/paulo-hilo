2ca2dd09a14da97be2262bff93568a06b54a5e7d
Mark Paulo Cruz <92324593+MarkPaulo10@users.noreply.github.com>
2025-01-25 16:00:46 +0800

    form update

---
+++ hilo/src/components/Steps.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="steps">
+        <div :class="`step ${getClass(index)}`" v-for="index in 6" :key="index">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        step: {
+            type: Number,
+            required: true
+        }
+    },
+
+    methods: {
+        getClass(index) {
+            return ((index === this.step)
+                ? 'active'
+                : (index < this.step)
+                    ? 'completed' : '');
+        }
+    }
+}
+</script>
+<style scoped>
+.steps {
+    width: 100%;
+    height: 3px;
+    display: grid;
+    gap: 0.5rem;
+    grid-template-columns: repeat(6, 1fr);
+}
+.step {
+    width: 60px;
+    height: 100%;
+    border-radius: 5px;
+    background: #F5F5F5;
+}
+.active {
+    background-color: var(--vt-c-text-primary);
+}
+.completed {
+    background-color: var(--vt-c-text-secondary);
+}
+</style>
